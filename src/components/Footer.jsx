4ea6b3566735c0b46509a0ab3683dd827a702546
Oslonline <Oslo418@yandex.ru>
2024-09-25 20:42:55 +0200

    fix: change 2024 to current year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="mt-14 flex w-full items-center justify-center p-4">
       <div className="flex w-full justify-around font-mono">
         <p>Oslo418's portfolio</p>
-        <p>2024</p>
+        <p>{new Date().getFullYear()}</p>
       </div>
     </footer>
   );
