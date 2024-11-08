7aeb9f1c24e432c8985cf62abe632c2e26b51a99
Oslonline <Oslo418@yandex.ru>
2024-11-09 00:31:28 +0100

    fix: minor styling changes

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -12,7 +12,10 @@ const ProjectCard = ({ p }) => {
 
   return (
     <a href={p.link} className="group relative flex flex-col gap-2 self-start rounded-md bg-gray-50 p-4 shadow-sm duration-150 hover:shadow-md md:hover:scale-[1.02]">
-      <div className="relative h-28 lg:h-36 xl:h-44 4xl:h-56 w-full rounded-t-md bg-cover bg-center bg-no-repeat filter duration-150 group-hover:filter-none md:grayscale" style={{ backgroundImage: `url(${p.card_img_src})` }}>
+      <div
+        className="relative h-28 w-full rounded-t-md bg-cover bg-center bg-no-repeat filter duration-150 group-hover:filter-none md:grayscale lg:h-36 xl:h-40"
+        style={{ backgroundImage: `url(${p.card_img_src})` }}
+      >
         <span className={`absolute right-2 top-2 rounded-full px-1.5 py-[1px] text-sm ${getBadgeStyle(p.type)}`}>{p.type}</span>
       </div>
       <div className="flex flex-col">
