481bc43bd1a4cae8280dfdc5350a32f44948c189
Oslonline <Oslo418@yandex.ru>
2025-04-15 20:30:40 +0200

    fix: adjust layout toggle button styles for better visibility

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,12 @@ function App() {
 
   return (
     <div className="relative">
-      <label className="fixed right-5 bottom-4 z-50 flex cursor-pointer flex-col items-center gap-1">
+      <label className="fixed right-3 bottom-3 z-50 flex cursor-pointer flex-col items-center gap-1 md:right-7 md:bottom-7">
         <div className={`text-xs text-zinc-500`}>{isFancy ? "Fancy" : "Minimalist"}</div>
         <div className="relative">
           <input type="checkbox" className="sr-only" checked={isFancy} onChange={toggleLayout} />
-          <div className={`block h-4 w-16 rounded-full transition-colors duration-200 ${isFancy ? "bg-cyan-500" : "bg-gray-600"}`}></div>
-          <div className={`dot absolute top-1 left-1 h-2 w-6 rounded-full bg-white transition-transform duration-200 ${isFancy ? "translate-x-8" : "translate-x-0"}`}></div>
+          <div className={`block h-6 w-16 rounded-full transition-colors duration-200 ${isFancy ? "bg-cyan-500" : "bg-gray-600"}`}></div>
+          <div className={`dot absolute top-1 left-1 h-4 w-6 rounded-full bg-white transition-transform duration-200 ${isFancy ? "translate-x-8" : "translate-x-0"}`}></div>
         </div>
       </label>
       {isFancy ? <FancyLayout /> : <MinimalistLayout />}
