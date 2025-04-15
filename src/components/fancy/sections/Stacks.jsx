2a7cb6c65aaf9771e2a082eae1e4b69df7651933
Oslonline <Oslo418@yandex.ru>
2025-04-15 20:33:19 +0200

    feat: update Stacks component by adding TypeScript and Supabase, and removing Bootstrap and PHP

--- src/components/fancy/sections/Stacks.jsx
+++ src/components/fancy/sections/Stacks.jsx
@@ -1,30 +1,28 @@
-import { FaReact, FaNodeJs, FaPhp, FaGithub, FaGitAlt } from "react-icons/fa";
-import { FaBootstrap, FaSquarespace, FaSymfony, FaVuejs, FaWordpress } from "react-icons/fa6";
+import { FaReact, FaNodeJs, FaGithub, FaGitAlt } from "react-icons/fa";
+import { FaSquarespace, FaVuejs, FaWordpress } from "react-icons/fa6";
 import { FiFigma } from "react-icons/fi";
 import { RiSupabaseLine } from "react-icons/ri";
-import { SiNextdotjs, SiTailwindcss, SiExpress, SiMysql, SiPostgresql, SiMongodb, SiJavascript } from "react-icons/si";
+import { SiNextdotjs, SiTailwindcss, SiExpress, SiMysql, SiPostgresql, SiMongodb, SiJavascript, SiTypescript } from "react-icons/si";
 
 export default function Stacks() {
   const stacks = {
     frontend: [
       { name: "Javascript", icon: <SiJavascript className="h-6 w-6" /> },
+      { name: "Typescript", icon: <SiTypescript className="h-6 w-6" /> },
       { name: "React", icon: <FaReact className="h-6 w-6" /> },
       { name: "Next.js", icon: <SiNextdotjs className="h-6 w-6" /> },
       { name: "Vue.js", icon: <FaVuejs className="h-6 w-6" /> },
       { name: "TailwindCSS", icon: <SiTailwindcss className="h-6 w-6" /> },
-      { name: "Bootstrap", icon: <FaBootstrap className="h-6 w-6" /> },
     ],
     backend: [
       { name: "Node.js", icon: <FaNodeJs className="h-6 w-6" /> },
       { name: "Express", icon: <SiExpress className="h-6 w-6" /> },
-      { name: "PHP", icon: <FaPhp className="h-6 w-6" /> },
-      { name: "Symfony", icon: <FaSymfony className="h-6 w-6" /> },
     ],
     database: [
+      { name: "Supabase", icon: <RiSupabaseLine className="h-6 w-6" /> },
       { name: "MySQL", icon: <SiMysql className="h-6 w-6" /> },
       { name: "PostgreSQL", icon: <SiPostgresql className="h-6 w-6" /> },
       { name: "MongoDB", icon: <SiMongodb className="h-6 w-6" /> },
-      { name: "Supabase", icon: <RiSupabaseLine className="h-6 w-6" /> },
     ],
     cms: [
       { name: "Wordpress", icon: <FaWordpress className="h-6 w-6" /> },
@@ -46,7 +44,7 @@ export default function Stacks() {
           {Object.keys(stacks).map((category) => (
             <div key={category} className="flex flex-wrap justify-center gap-4 md:justify-start">
               {stacks[category].map(({ name, icon }) => (
-                <div key={name} className="flex select-none items-center gap-2 rounded-md bg-gray-50 px-4 py-2 text-center text-lg font-medium shadow-xs">
+                <div key={name} className="flex items-center gap-2 rounded-md bg-gray-50 px-4 py-2 text-center text-lg font-medium shadow-xs select-none">
                   {icon}
                   <span>{name}</span>
                 </div>
